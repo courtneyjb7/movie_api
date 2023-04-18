0e025d094c4fea2890b4ccd74a41ddffb5ab7191
courtneyjb7 <72003830+courtneyjb7@users.noreply.github.com>
2023-04-17 20:13:22 -0700

    lines

--- src/api/lines.py
+++ src/api/lines.py
@@ -4,24 +4,90 @@ from src import database as db
 
 router = APIRouter()
 
+
+@router.get("/lines/conv/{conv_id}", tags=["lines"])
+def get_char_conversations(conv_id: int):
+    """
+    This endpoint returns a conversation by its identifier. For the conversation it returns:
+    * `movie_title`: The title of the movie the conversation is in.
+    * `ch1`: the name of one character in the conversation.
+    * `ch2`: the name of the other character in the conversation.
+    * `lines`: a dictionary of lines that make up the conversation.
+
+    Each line is represented by a dictionary with the following keys:
+    * `character`: the name of the character saying the line.
+    * `line`: the text of the line.
+
+    """
+    conv = db.conversations.get(conv_id)
+    conv_lines = filter(
+            lambda l: l.conv_id == conv_id,
+            db.lines.values(),
+        )
+    if conv:
+        result = {
+            "movie_title": db.movies[conv.movie_id].title,
+            "ch1": db.characters[conv.c1_id].name,
+            "ch2": db.characters[conv.c2_id].name,
+            "lines": (
+                {
+                    "character": db.characters[line.c_id].name,
+                    "line": line.line_text
+                }
+                for line in conv_lines
+            ),
+        }
+    
+        return result
+
+    raise HTTPException(status_code=404, detail="movie not found.")
+
+
 @router.get("/lines/{line_id}", tags=["lines"])
 def get_line(line_id: int):
     """
-    This endpoint returns a single movie by its identifier. For each movie it returns:
-    * `movie_id`: the internal id of the movie.
-    * `title`: The title of the movie.
-    * `top_characters`: A list of characters that are in the movie. The characters
-      are ordered by the number of lines they have in the movie. The top five
-      characters are listed.
-
-    Each character is represented by a dictionary with the following keys:
-    * `character_id`: the internal id of the character.
-    * `character`: The name of the character.
-    * `num_lines`: The number of lines the character has in the movie.
+    This endpoint returns a single line by its identifier. For each line it returns:
+    * `line_id`: the internal id of the line.
+    * `character_name`: the name of the character saying the line
+    * `movie_title`: The title of the movie the line is from.
+    * `text`: the text of the line
+    * `conv_id`: the id of the conversation the line is in
+    * `other_character_name`: the name of the other character in the conversation
+    * `num_conv_btw_chars`: the number of conversations between these characters in the movie
+    * `conversation`: list of the rest of the lines in the conversation
 
     """
-    json = "hello"
-    return json
+    line = db.lines.get(line_id)
+    if line:
+        conv_lines = filter(
+            lambda l: l.conv_id == line.conv_id,
+            db.lines.values(),
+        )
+
+        conv = db.conversations[line.conv_id]
+        character = db.characters[line.c_id].name
+        result = {
+            "line_id": line_id,
+            "character_name": character,
+            "movie_title": db.movies[line.movie_id].title,
+            "text": line.line_text,
+            "conv_id": line.conv_id,
+            "other_character_name": db.characters[conv.c1_id].name or None
+                                   if db.characters[conv.c1_id].name != character
+                                   else db.characters[conv.c2_id].name or None,
+            "num_conv_btw_chars": len(list(filter(
+                                    lambda conv: conv.movie_id == line.movie_id
+                                    and (conv.c1_id == line.c_id or conv.c2_id == line.c_id),
+                                    db.conversations.values(),
+                                ))),
+            "conversation": [
+                conv.line_text
+                for conv in conv_lines
+            ]
+        }
+        return result
+
+    raise HTTPException(status_code=404, detail="line not found.")
 
 
 class lines_sort_options(str, Enum):
@@ -35,7 +101,7 @@ def list_movies(
     name: str = "",
     limit: int = 50,
     offset: int = 0,
-    sort: lines_sort_options = lines_sort_options.character_name,
+    sort: lines_sort_options = lines_sort_options.movie_title,
 ):
     """
     This endpoint returns a list of lines. For each line it returns:
@@ -46,17 +112,12 @@ def list_movies(
     * `line_sort`: The order the line is said in the conversation.
     * `line_text`: The line itself.
 
-    You can filter for movies whose titles contain a string by using the
+    You can filter for lines whose text contains a string by using the
     `name` query parameter.
 
     You can also sort the results by using the `sort` query parameter:
     * `movie_title` - Sort by movie title alphabetically.
     * `character_name` - Sort by character name alphabetically.
-
-    The `limit` and `offset` query
-    parameters are used for pagination. The `limit` query parameter specifies the
-    maximum number of results to return. The `offset` query parameter specifies the
-    number of results to skip before returning results.
     """
     if name:
 
@@ -73,8 +134,6 @@ def list_movies(
         items.sort(key=lambda l: l.movie_id)
     elif sort == lines_sort_options.character_name:
         items.sort(key=lambda l: l.c_id)
-    # elif sort == lines_sort_options.rating:
-    #     items.sort(key=lambda m: m.imdb_rating, reverse=True)
 
     json = (
         {
---
+++ test/test_lines.py
@@ -0,0 +1,66 @@
+from fastapi.testclient import TestClient
+
+from src.api.server import app
+
+import json
+
+client = TestClient(app)
+
+
+def test_get_line():
+    response = client.get("/lines/238")
+    assert response.status_code == 200
+
+    with open("test/lines/238.json", encoding="utf-8") as f:
+        assert response.json() == json.load(f)
+
+
+def test_lines():
+    response = client.get("/lines/")
+    assert response.status_code == 200
+
+    with open("test/lines/root.json", encoding="utf-8") as f:
+        assert response.json() == json.load(f)
+
+
+def test_sort_filter():
+    response = client.get(
+        "/lines/?name=hello&limit=10&offset=20&sort=character_name"
+    )
+    assert response.status_code == 200
+
+    with open(
+        "test/lines/lines-name=hello&limit=10&offset=20&sort=character_name.json",
+        encoding="utf-8",
+    ) as f:
+        assert response.json() == json.load(f)
+
+def test_sort_filter2():
+    response = client.get(
+        "/lines/?name=what&limit=15&offset=60&sort=movie_title"
+    )
+    assert response.status_code == 200
+
+    with open(
+        "test/lines/lines-name=what&limit=15&offset=60&sort=movie_title.json",
+        encoding="utf-8",
+    ) as f:
+        assert response.json() == json.load(f)
+
+
+def test_conv():
+    response = client.get(
+        "/lines/conv/500"
+    )
+    assert response.status_code == 200
+
+    with open(
+        "test/lines/conv-500.json",
+        encoding="utf-8",
+    ) as f:
+        assert response.json() == json.load(f)
+
+
+def test_404():
+    response = client.get("/lines/400")
+    assert response.status_code == 404
